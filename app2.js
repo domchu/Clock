de81e8dae4c4c98b62e93685111b989978bbfa28
Mr Dominic <ochu.dominic.onah@gmail.com>
2022-07-02 14:11:35 +0100

    added the days of the week

--- app2.js
+++ app2.js
@@ -5,6 +5,11 @@ setInterval(() => {
   let minute = date.getMinutes();
   let seconds = date.getSeconds();
   let am_pm;
+  let currentDay;
+  let day = date.getDay();
+  let month = date.getMonth();
+  let year = date.getFullYear();
+
   am_pm = hour >= 12 ? "PM" : "AM";
 
   if (hour < 10) {
@@ -21,5 +26,34 @@ setInterval(() => {
     hour = hour - 12;
   }
 
+  switch (date.getDay()) {
+    case 0:
+      currentDay = "Sunday";
+      break;
+    case 1:
+      currentDay = "Monday";
+      break;
+    case 2:
+      currentDay = "Tueday";
+      break;
+    case 3:
+      currentDay = "Wednesday";
+      break;
+    case 4:
+      currentDay = "Thursday";
+      break;
+    case 5:
+      currentDay = "Friday";
+      break;
+    case 6:
+      currentDay = "Saturday";
+      break;
+
+    default:
+      break;
+  }
+  // console.log(currentDay);
+
   clock.innerHTML = `${hour} : ${minute} : ${seconds} ${am_pm}`;
+  document.querySelector(".day").innerHTML = currentDay;
 }, 1000);
