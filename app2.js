f7f8855bd04c1acb34c409625cf78889d07fb9cc
Mr Dominic <ochu.dominic.onah@gmail.com>
2022-07-02 14:29:42 +0100

    added the month

--- app2.js
+++ app2.js
@@ -52,9 +52,51 @@ setInterval(() => {
     default:
       break;
   }
-  // console.log(currentDay);
+  switch (date.getFullYear()) {
+    case 0:
+      month = "January";
+      break;
+    case 1:
+      month = "Febuary";
+      break;
+    case 2:
+      month = " March";
+      break;
+    case 3:
+      month = "April";
+      break;
+    case 4:
+      month = "May";
+      break;
+    case 5:
+      month = "June";
+      break;
+    case 6:
+      month = "July";
+      break;
+    case 7:
+      month = "August";
+      break;
+    case 8:
+      month = "September";
+      break;
+    case 9:
+      month = "October";
+      break;
+    case 10:
+      month = "November";
+      break;
+    case 11:
+      month = "December";
+      break;
+
+    default:
+      break;
+  }
+  // console.log(month);`
 
   clock.innerHTML = `${hour} : ${minute} : ${seconds} ${am_pm}`;
   document.querySelector(".day").innerHTML = currentDay;
-  document.querySelector(".year").innerHTML = year;
+  document.querySelector(".month").innerHTML = `${month}`;
+  document.querySelector(".year").innerHTML = `${year}`;
 }, 1000);
